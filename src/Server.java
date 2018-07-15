4f9abe8761891f4a089789542e2b04eb855645f2
League <league@WTS9.attlocal.net>
2018-07-15 14:25:54 -0700

    Got up to date
    Got up to date

--- src/Server.java
+++ src/Server.java
@@ -1,4 +1,6 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -35,35 +37,76 @@ class ThreadedSocket extends Thread {
             String request_method = line;
             System.out.println(line);
             line = "";
-            int postDataI = -1;
-            while ((line = in.readLine()) != null && (line.length() != 0)) {
-                System.out.println(line);
-                if (line.indexOf("Content-Length:") > -1) {
-                    postDataI = new Integer(
-                            line.substring(
-                                    line.indexOf("Content-Length:") + 16,
-                                    line.length())).intValue();
-                }
-            }
-            String postData = "";
-  
-            if (postDataI > 0) {
-                char[] charArray = new char[postDataI];
-                in.read(charArray, 0, postDataI);
-                postData = new String(charArray);
-            }
-          
-            out.println("HTTP/1.0 200 OK");
-            out.println("Content-Type: text/html; charset=utf-8");
-            out.println("Server: MINISERVER");
-            out.println("");
-            out.println("<h1>Welcome to the Web Site</h1>");
-            out.println("Username: <input></input><br>Password: <input type='password'></input>");
-            out.println("<button>submit</button>");
+            request_method = request_method.replaceAll("GET", "").replaceAll("/", "").trim().split(" ")[0];
+//            int postDataI = -1;
+////            while ((line = in.readLine()) != null && (line.length() != 0)) {
+////                System.out.println(line);
+////                if (line.indexOf("Content-Length:") > -1) {
+////                    postDataI = new Integer(
+////                            line.substring(
+////                                    line.indexOf("Content-Length:") + 16,
+////                                    line.length())).intValue();
+////                }
+////            }
+////            String postData = "";
+//  
+//            if (postDataI > 0) {
+//                char[] charArray = new char[postDataI];
+//                in.read(charArray, 0, postDataI);
+//                postData = new String(charArray);
+//            }
+            BufferedReader br;
+    			if(request_method.equalsIgnoreCase("wave")) {
+    				br = new BufferedReader(new FileReader("/Users/league/Desktop/wave.html"));
+    				String line2 = "";
+    				out.println("HTTP/1.0 200 OK");
+    	            out.println("Content-Type: text/html; charset=utf-8");
+    	            out.println("Server: MINISERVER");
+    	            out.println("");
+    				while((line2 = br.readLine()) != null) {
+    					out.println(line2);
+    				}
+    			}
+    			else if(request_method.equalsIgnoreCase("free")) {
+    				br = new BufferedReader(new FileReader("/Users/league/Desktop/free.html"));
+    				String line2 = "";
+    				out.println("HTTP/1.0 200 OK");
+    	            out.println("Content-Type: text/html; charset=utf-8");
+    	            out.println("Server: MINISERVER");
+    	            out.println("");
+    				while((line2 = br.readLine()) != null) {
+    					out.println(line2);
+    				}
+    			}
+    			else {
+    				out.println("HTTP/1.0 200 OK");
+    	            out.println("Content-Type: text/html; charset=utf-8");
+    	            out.println("Server: MINISERVER");
+    	            out.println("");
+    	            out.println("<h1>Welcome to the Web Site</h1>");
+    	            out.println("Username: <input></input><br>Password: <input type='password'></input>");
+    	            out.println("<script>function red() {window.location = \"https://www.google.com/\"; } </script>");
+    	       
+    	            out.println("<button onClick=\"red()\">Button</button>");
+    	            
+    			}
             out.close();
             insocket.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+    private void loadPage(String request, PrintWriter printWriter) throws IOException {
+    		BufferedReader br;
+    		if(request.equalsIgnoreCase("wave")) {
+    			br = new BufferedReader(new FileReader("/Users/league/Desktop/wave.html"));
+    			String line = "";
+    			while((line = br.readLine()) != null) {
+    				printWriter.println(line);
+    			}
+    		}
+    		else {
+    			
+    		}
     }
 }
